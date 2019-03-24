5e8096d658bb576bcfabdc0fc7ac983d74e9474f
OmniOwl <xenon373@gmail.com>
2019-03-24 19:09:42 +0100

    Basic Setup Commit

---
+++ BananaBot/BananaBot/Services/CommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace BananaBot.Services
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _discord;
+        private readonly CommandService _commands;
+        private readonly IConfigurationRoot _config;
+        private readonly IServiceProvider _provider;
+
+        // DiscordSocketClient, CommandService, IConfigurationRoot, and IServiceProvider are injected automatically from the IServiceProvider
+        public CommandHandler(
+            DiscordSocketClient discord,
+            CommandService commands,
+            IConfigurationRoot config,
+            IServiceProvider provider)
+        {
+            _discord = discord;
+            _commands = commands;
+            _config = config;
+            _provider = provider;
+
+            _discord.MessageReceived += OnMessageReceivedAsync;
+        }
+
+        private async Task OnMessageReceivedAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;     // Ensure the message is from a user/bot
+            if (msg == null) return;
+            if (msg.Author == _discord.CurrentUser) return;     // Ignore self when checking commands
+
+            var context = new SocketCommandContext(_discord, msg);     // Create the command context
+
+            int argPos = 0;     // Check if the message has a valid command prefix
+            if (msg.HasStringPrefix(_config["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _provider);     // Execute the command
+
+                if (!result.IsSuccess && msg.Channel.Name.Contains("bot-test-server"))     // If not successful, reply with the error.
+                    await context.User.SendMessageAsync(result.ToString());
+            }
+        }
+    }
+}
---
+++ BananaBot/BananaBot/Utility.cs
@@ -0,0 +1,267 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  © Mads Falkenberg Sønderstrup, Denmark, 2017   *
+ *  All Rights Reserved.                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+using Discord;
+
+namespace BananaBot
+{
+    public static class Utility
+    {
+        private static Dictionary<String, ulong> _serverids;
+
+        /// <summary>
+        /// Used to initialize any values this static class might need.
+        /// Should only be run once, from the main method.
+        /// </summary>
+        public static void Init()
+        {
+            JObject jsonObj = JObject.Parse(File.ReadAllText(AppContext.BaseDirectory + "_configuration.json"));
+            JToken jToken = jsonObj.SelectToken("serverID");
+            _serverids = new Dictionary<String, ulong>();
+            foreach (JProperty obj in jToken.Children())
+            {
+                _serverids.Add(obj.Name, ulong.Parse((String)obj.Value));
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if the serverID provided matches a server from our configuration.
+        /// </summary>
+        /// <param name="serverId"></param>
+        /// <param name="serverName"></param>
+        /// <returns>True if the server ID matches the ID of the server requested, else False in all other cases.</returns>
+        public static bool CompareServerIds(ulong serverId, String serverName)
+        {
+            if (_serverids.ContainsKey(serverName))
+            {
+                if (_serverids[serverName] == serverId)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Used to find all users with a single role as search query.
+        /// </summary>
+        /// <param name="role">The role to look for.</param>
+        /// <param name="context">The command context currently in use</param>
+        /// <param name="includeOffline">Optional parameter to include offline users. Defaults to false.</param>
+        /// <returns>A list of users that have the mentioned role.</returns>
+        public async static Task<List<SocketGuildUser>> FindAllUsersWithRole(String role, ICommandContext context, bool includeOffline = false)
+        {
+            List<SocketGuildUser> data = new List<SocketGuildUser>();
+            IReadOnlyCollection<SocketGuildUser> result = await context.Guild.GetUsersAsync() as IReadOnlyCollection<SocketGuildUser>;
+            if (includeOffline == true)
+            {
+                foreach (SocketGuildUser user in result)
+                {
+                    foreach (SocketRole sRole in user.Roles)
+                    {
+                        if (sRole.Name.Equals(role))
+                        {
+                            data.Add(user);
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (SocketGuildUser user in result)
+                {
+                    if (user.Status == Discord.UserStatus.Online || user.Status == Discord.UserStatus.Idle || user.Status == Discord.UserStatus.AFK)
+                    {
+                        foreach (SocketRole sRole in user.Roles)
+                        {
+                            if (sRole.Name.Equals(role))
+                            {
+                                data.Add(user);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Used to find all users with a single role as search query and exludes a role from the result.
+        /// </summary>
+        /// <param name="role">The role to look for.</param>
+        /// <param name="context">The command context currently in use</param>
+        /// <param name="exclude">The role to exclude.</returns>
+        public async static Task<List<SocketGuildUser>> FindAllUsersWithRoleExclude(String role, ICommandContext context, String exclude)
+        {
+            List<SocketGuildUser> data = new List<SocketGuildUser>();
+            IReadOnlyCollection<SocketGuildUser> result = await context.Guild.GetUsersAsync() as IReadOnlyCollection<SocketGuildUser>;
+            List<SocketGuildUser> filteredUsers = result.ToList();
+
+            for (int i = 0; i < filteredUsers.Count; i++)
+            {
+                foreach (SocketRole sRole in filteredUsers[i].Roles)
+                {
+                    if (sRole.Name.Equals(exclude))
+                    {
+                        filteredUsers.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            foreach (SocketGuildUser user in filteredUsers)
+            {
+                foreach (SocketRole sRole in user.Roles)
+                {
+                    if (sRole.Name.Equals(role))
+                    {
+                        data.Add(user);
+                        break;
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Used to find all users with a single role as search query.
+        /// </summary>
+        /// <param name="role">The role to look for.</param>
+        /// <param name="users">The List of Users to look through for the mentioned role.</param>
+        /// <returns>A list of users that have the mentioned role.</returns>
+        public static List<SocketGuildUser> FindAllUsersWithRole(String role, List<SocketGuildUser> users)
+        {
+            List<SocketGuildUser> data = new List<SocketGuildUser>();
+            foreach (SocketGuildUser user in users)
+            {
+                foreach (SocketRole sRole in user.Roles)
+                {
+                    if (role.Equals(sRole.Name.ToLower()))
+                    {
+                        data.Add(user);
+                        break;
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Used to find all users with multiple roles as search query.
+        /// </summary>
+        /// <param name="roles">The roles to look for in comma seperated string.</param>
+        /// <param name="users">The List of Users to look through for the mentioned roles.</param>
+        /// <param name="isExplicitSearch">If this is set to false, then anyone with one of the roles is returned. If True only those with all the roles are returned.</param>
+        /// <returns>A list of users that have one or more of the mentioned roles.</returns>
+        public static List<SocketGuildUser> FindAllUsersWithRoles(String roles, List<SocketGuildUser> users, bool isExplicitSearch = true)
+        {
+            List<SocketGuildUser> data = new List<SocketGuildUser>();
+            roles = roles.ToLower();
+            // Split the list of roles to look for using the comma as the delimiter.
+            String[] rolesToCheck = roles.Split(",");
+            if (isExplicitSearch == true)
+            {
+                foreach (SocketGuildUser user in users)
+                {
+                    List<String> roleList = new List<String>();
+                    int checkSum = 0;
+                    // Get all the roles the user got in string format
+                    // so we can compare them to the roles we are looking for
+                    foreach (SocketRole role in user.Roles)
+                    {
+                        roleList.Add(role.Name);
+                    }
+                    // Check if we can find any matches.
+                    foreach (String sRole in rolesToCheck)
+                    {
+                        if (roleList.Contains(sRole.ToLower()))
+                        {
+                            checkSum++;
+                        }
+                    }
+                    // If we found as many matching roles as there are roles in
+                    // our control list, then we are good and we add them to the
+                    // return list of data.
+                    if (checkSum == (rolesToCheck.Length))
+                    {
+                        data.Add(user);
+                    }
+                }
+            }
+            else
+            {
+                foreach (SocketGuildUser user in users)
+                {
+                    foreach (SocketRole sRole in user.Roles)
+                    {
+                        if (roles.Contains(sRole.Name.ToLower()))
+                        {
+                            data.Add(user);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Used to find the Hierarchy Value so that users can be compared by rank.
+        /// </summary>
+        /// <param name="user">The user whom should be evaluated</param>
+        /// <returns>A number representing their place in the hierarchy. Higher Number has more previleges.</returns>
+        public static EHierarchy GetUserHierarchyValue(SocketGuildUser user)
+        {
+            List<String> Roles = new List<String>();
+            foreach (IRole role in user.Roles)
+            {
+                Roles.Add(role.Name);
+            }
+
+            if (Roles.Contains("Administrator") || Roles.Contains("Admin"))
+            {
+                return EHierarchy.ADMINISTRATOR;
+            }
+            else if (Roles.Contains("Bot Master"))
+            {
+                return EHierarchy.BOTMASTER;
+            }
+            else if (Roles.Contains("Supervisor"))
+            {
+                return EHierarchy.SUPERVISOR;
+            }
+            else if (Roles.Contains("Moderator"))
+            {
+                return EHierarchy.MODERATOR;
+            }
+            else
+            {
+                return (int)EHierarchy.USER;
+            }
+        }
+    }
+}
