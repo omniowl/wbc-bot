c247a0d8a5e62120e1b0fad30106e1fde362590b
OmniOwl <xenon373@gmail.com>
2019-03-24 19:14:57 +0100

    Started work on FindCoach command

---
+++ BananaBot/BananaBot/Modules/FindCoach.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananaBot.Modules
+{
+    public class FindCoach : ModuleBase
+    {
+        [Command("find"), Summary("Used to find a Coach.")]
+        public async Task Find(string commandText)
+        {
+
+        }
+    }
+}
